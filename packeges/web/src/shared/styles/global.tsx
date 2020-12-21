05f91443e1199a7ece06aaa1560335bb16e91e57
Arthur Costa da Silva <arthur.silva@pampa.compasso>
2020-12-21 16:00:49 -0300

    chore: change styled
    web

---
+++ packeges/web/src/shared/styles/global.tsx
@@ -0,0 +1,24 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  *, *::before, *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :root {
+    --header-height: 4rem;
+    --footer-height: 2rem;
+    --navleft-width-close: 6rem;
+    --navleft-width-open: 13.6rem;
+  }
+
+  body {
+    font-size: 1rem;
+    font-family: 'Roboto', sans-serif;
+    background: ${props => props.theme.bg.body};
+    color: ${props => props.theme.fg.body};
+    transition: background .6s ease-in-out, color .6s  ease-in-out;
+  }
+`
